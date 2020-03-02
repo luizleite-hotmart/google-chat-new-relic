64b181b712aa47d6e7d0521e17bd0870dc17b252
Luiz Leite Oliveira <luiz.leite@hotmart.com>
2020-03-02 20:36:28 -0300

    add unversioned files

---
+++ src/main/kotlin/com/luizleiteoliveira/googlechatnewrelicalert/GoogleChatNewRelicAlertApplication.kt
@@ -0,0 +1,11 @@
+package com.luizleiteoliveira.googlechatnewrelicalert
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoogleChatNewRelicAlertApplication
+
+fun main(args: Array<String>) {
+    runApplication<GoogleChatNewRelicAlertApplication>(*args)
+}
---
+++ src/test/kotlin/com/luizleiteoliveira/googlechatnewrelicalert/GoogleChatNewRelicAlertApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.luizleiteoliveira.googlechatnewrelicalert
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoogleChatNewRelicAlertApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
