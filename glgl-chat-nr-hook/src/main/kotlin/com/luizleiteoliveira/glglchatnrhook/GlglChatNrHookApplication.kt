936425dd0226275bcc41585a9db8ce13e101b1f0
Luiz Leite Oliveira <luiz.leite@hotmart.com>
2020-03-02 19:11:28 -0300

    start project

---
+++ glgl-chat-nr-hook/src/main/kotlin/com/luizleiteoliveira/glglchatnrhook/GlglChatNrHookApplication.kt
@@ -0,0 +1,11 @@
+package com.luizleiteoliveira.glglchatnrhook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GlglChatNrHookApplication
+
+fun main(args: Array<String>) {
+	runApplication<GlglChatNrHookApplication>(*args)
+}
